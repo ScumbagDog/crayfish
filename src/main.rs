62843463079294b8ce540ea64fc9c495f99d78b8
ScumbagDog <lbrack17@student.aau.dk>
2020-10-14 13:01:26 +0200

    tilføjede funktion til at sætte ordrer/jobs ind i orderkøen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde::{Serialize, Deserialize};
 use tide::prelude::*;
 use std::sync::{Arc, RwLock};
 
@@ -13,7 +14,7 @@ struct Order {
 }
 
 #[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> std::result::Result<(), std::io::Error> {
     let state = State {
         jobqueue: Arc::new(RwLock::new(Vec::new())),
     };
@@ -21,19 +22,32 @@ async fn main() -> Result<(), std::io::Error> {
     let mut app = tide::with_state(state);
     app.at("/").get(process_get);
     app.at("/").post(process_post);
+    app.at("/list").get(process_list);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
 async fn process_post(mut req: tide::Request<State>) -> tide::Result {
-    let order: Order = req.body_form().await?;
+    let order: Order = req.body_json().await?;
     req.state().jobqueue.write().unwrap().push(order);
     Ok(tide::Response::new(200))
 }
 
 async fn process_get(mut req: tide::Request<State>) -> tide::Result {
+    let orderqueue = &mut *req.state().jobqueue.write().unwrap();
+    let order = orderqueue.pop();
+    match order {
+        Some(order) => Ok(tide::Response::builder(200)
+                            .body(tide::Body::from_json(&order)?)
+                            .build()),
+        None => Ok(tide::Response::builder(204)
+                .build()),
+    }
+}
+
+async fn process_list(req: tide::Request<State>) -> tide::Result {
     let orderqueue = &*req.state().jobqueue.read().unwrap();
     Ok(tide::Response::builder(200)
-       .body(tide::Body::from_json(orderqueue)?)
+       .body(tide::Body::from_json(&orderqueue)?)
        .build())
 }
